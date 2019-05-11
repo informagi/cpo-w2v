fb947b698040ced5a8f9bc0073d8847f0dea3c5b
Arjen P. de Vries <arjen@acm.org>
2019-05-11 13:02:18 +0200

    Create a hashed password for starting Jupyter notebook in Docker

---
+++ pwd.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys, getopt
+from IPython.lib import passwd
+
+def main(argv):
+   mypwd = 'copd';
+   try:
+      opts, args = getopt.getopt(argv,"hp:",["pwd="])
+   except getopt.GetoptError:
+      print 'pwd.py -p password'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -p <password>'
+         sys.exit()
+      elif opt in ("-p", "--pwd"):
+         mypwd = arg
+      print passwd(mypwd)
+         
+if __name__ == "__main__":
+   main(sys.argv[1:])
