6a581190559bc3a0f0595eb36358940305b8f31a
Arjen P. de Vries <arjen@acm.org>
2021-10-13 21:58:19 +0200

    fixes

--- pwd.py
+++ pwd.py
@@ -8,15 +8,15 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hp:",["pwd="])
    except getopt.GetoptError:
-      print 'pwd.py -p password'
+      print('pwd.py -p password')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -p <password>'
+         print('test.py -p <password>')
          sys.exit()
       elif opt in ("-p", "--pwd"):
          mypwd = arg
-   print "'{}'".format(passwd(mypwd))
+   print("'{}'".format(passwd(mypwd)))
          
 if __name__ == "__main__":
    main(sys.argv[1:])
